dc894ce737fe4c7edfc2a8d89837c7fb9aca2c26
Greg <47268138+gregsypek@users.noreply.github.com>
2024-10-17 15:57:30 +0200

    handle form errors

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { lusitana } from "@/app/ui/fonts";
 
-import styles from "@/app/ui/home.module.css";
 import Image from "next/image";
 
 export default function Page() {
